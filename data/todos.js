e0731ebce90b61f4d56610813765bfe37c224dca
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-10 17:24:33 -0800

    edited data to todos from animals

---
+++ data/todos.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    todo: 'clean car',
+    is_completed: false
+  },
+  {
+    todo: 'make dinner',
+    is_completed: false,
+  },
+  {
+
+    todo: 'change baby',
+    is_completed: false,
+  }
+];
